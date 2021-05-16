8fa6537f83b8c213f75830ac7026b223a0e0ee60
Remn <gameremn@mail.ru>
2021-05-16 23:02:13 +0300

    Add new image on menu

---
+++ Assets/Scripts/Input/ButtonDownAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonDownAction : MonoBehaviour
+{
+    public KeyCode kay;
+    
+    void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true;
+    }
+    
+    public UnityEvent unityEvent;
+    private void Update()
+    {
+        if (Input.GetKeyDown(kay))
+            unityEvent.Invoke();
+    }
+}
